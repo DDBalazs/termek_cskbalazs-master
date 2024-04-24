9f2119c6904130afbd50a72855a5c3923a208daf
csoke.balazs <csoke.balazs@117-12>
2024-04-24 09:20:37 +0200

    Add project files.

---
+++ termek_cskbalazs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace termek_cskbalazs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cikkszám: ");
+            string cikkszam = Console.ReadLine();
+
+            Console.WriteLine("Megnevezése: ")
+            string megnevezes = Console.ReadLine();
+
+            Console.WriteLine("Beszerzési ár: ")
+            int beszerzesiar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ÁFA kulcs: ");
+            int afakulcs = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Akciós?");
+            string akcios = Console.ReadLine();
+
+            Console.WriteLine("Kedvezmény értéke: ")
+            int kedvezmeny = int.Parse(Console.ReadLine());
+
+
+            Termekek t = new Termek(cikkszam, megnevezes, beszerzesiar, afakulcs, akcios, kedvezmeny);
+            Console.WriteLine($"Nettó ár: {t.netto}")
+            Console.WriteLine($"Brutto ár: {t.brutto}");
+            
+            if( t.akcios = "Igen")
+            {
+                Console.WriteLine($"A {t.cikkszam} {t.megnevezes} jelengleg akciós")
+                Console.WriteLine($"Kedvezményes ár: {t.kedvezmeny}");
+            }
+            else
+            {
+                Console.WriteLine($"A {t.cikkszam} {t.megnevezes} jelengleg nem akciós")
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ termek_cskbalazs/Termek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace termek_cskbalazs
+{
+    internal class Termek
+    {
+        string cikkszam;
+        public string megnevezes;
+        public int beszerzesiar;
+        int afakulcs;
+        bool akcios;
+        int kedvezmény;
+        
+        public double netto()
+        {
+            double netto = Math.Round(beszerzesiar * 1.35, 2);
+            return netto;
+        }
+        public brutto()
+        {
+            int brutto =  Math.Round(netto * (100 + afakulcs) / 100,0);
+            return brutto;
+        }
+        public string akciose()
+        {
+            if (akcios == "Igen" || "igen")
+            {
+                Console.WriteLine($"A {cikkszam}, {megnevezes} jelenleg akciós");
+            }
+            else if (akcios == "Nem" || "nem")
+            {
+                Console.WriteLine($"A {cikkszam}, {megnevezes} jelenleg nem akciós");
+            }
+        }
+        public double kedvar()
+        {
+            double kedvar = Math.Round(brutto * (100 - kedvezmény) / 100,0);
+        }
+
+    }
+}
